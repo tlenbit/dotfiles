0c774dd2f76d3c37e0a94d3783d8cdc3931aac68
JCR <vonzo55@gmail.com>
2020-07-22 19:28:58 +0300

    tweaks for enabling apple keyboard and touchpad

--- i3/.config/i3/scripts/screen-setup.sh
+++ i3/.config/i3/scripts/screen-setup.sh
@@ -1,6 +1,7 @@
 # Monitor output
 if xrandr -q | grep 'HDMI-1 connected'; then
-	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+	#xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off
 fi
 
 # Wallpaper

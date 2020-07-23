0afbce89c8ec825081d19041e221c58b3e68e400
JCR <vonzo55@gmail.com>
2020-07-23 19:00:50 +0300

    moving touchpad specific commands to script

---
+++ i3/.config/i3/scripts/mac-controls.sh
@@ -0,0 +1,4 @@
+# MagicTrackpad
+xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Tapping Enabled" 1
+xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Click Method Enabled" 1 0

67cc32e161026147626c6c1803de565b49bb6346
JCR <vonzo55@gmail.com>
2020-08-07 21:49:32 +0300

    new experiments

--- scripts/.scripts/screen-setup.sh
+++ scripts/.scripts/screen-setup.sh
@@ -5,6 +5,7 @@ function set_monitors() {
 	if xrandr -q | grep 'HDMI-1 connected'; then
 		#xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
 		xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off
+		# xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 	else
 		xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 	fi
@@ -60,7 +61,7 @@ set_monitors
 
 run_polybar
 run_picom
-run_conky
+# run_conky
 
 set_still_wallpaper
 # set_animated_wallpaper

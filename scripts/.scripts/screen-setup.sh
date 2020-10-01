86a993c9985e8e16502dbc70fa71231743d1797d
JCR <vonzo55@gmail.com>
2020-10-01 21:14:43 +0300

    tweak

--- scripts/.scripts/screen-setup.sh
+++ scripts/.scripts/screen-setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 function set_monitors() {
-	# Monitor output
+	#xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
 	if xrandr -q | grep 'HDMI-1 connected'; then
 		#xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
 		xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off
-		# xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+		#xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 	else
 		xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 	fi
 
-	# xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
 }
 
 function set_still_wallpaper() {

c14fc1034632bcf324eb283dd998d412329be96a
JCR <vonzo55@gmail.com>
2020-12-08 13:08:21 +0200

    config tweaks

--- scripts/.scripts/screen-setup.sh
+++ scripts/.scripts/screen-setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-function set_monitors() {
-	#xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
-	if xrandr -q | grep 'HDMI-1 connected'; then
-		#xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
-		xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off
-		#xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
-	else
-		xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
-	fi
-
-}
 
 function set_still_wallpaper() {
 	# ps -ef | grep dwall | grep -v grep | awk '{print $2}' | xargs kill
@@ -58,8 +47,6 @@ function run_conky() {
 }
 
 # RUN SCREEN SETUP (order matters)
-set_monitors
-
 run_polybar
 run_picom
 # run_conky

1b03e0cc696ae1cdd742a0344a55b38b738e8a23
JCR <vonzo55@gmail.com>
2021-03-17 18:13:25 +0200

    general tweaks

--- scripts/.scripts/mac-controls.sh
+++ scripts/.scripts/mac-controls.sh
@@ -2,3 +2,5 @@
 xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Natural Scrolling Enabled" 1
 xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Tapping Enabled" 1
 xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Click Method Enabled" 1 0
+xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Middle Emulation Enabled" 1
+xinput set-prop "MagicTrackpad di RiccardoTommasini" "libinput Tapping Button Mapping Enabled" 1 0
--- scripts/.scripts/monitor-setup.sh
+++ scripts/.scripts/monitor-setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# External monitor name
-
-#xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
-if xrandr -q | grep 'HDMI-1 connected'; then
-	#xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
-	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off
-	#xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+if xrandr -q | grep -q 'DP-1 connected'; then
+	xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
 else
 	xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 fi

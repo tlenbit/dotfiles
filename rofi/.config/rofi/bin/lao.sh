73c6cfd83bd3952a87e8d35dba612444cad448ab
JCR <vonzo55@gmail.com>
2021-03-23 10:36:17 +0200

    now lao rofi can toggle favorite of a document with Ctrl+Alt+p

--- rofi/.config/rofi/bin/lao.sh
+++ rofi/.config/rofi/bin/lao.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
-collection_name="Computer Science"
+# documents=$(lao ls -s "-title" -f "{id}\t{title}\0icon\x1f{favorite?$icon_favorite:}" "$collections")
+# optional flag for showing more info:  -mesg "<span color='#636e72'>4 Documents | 5 Collections</span>"
+
+collections=("Computer Science" "Papers")
 icon_favorite="package_favorite"
 
-documents=$(lao ls -s "-title" -f "{id}\t{title}\0icon\x1f{favorite}" --format-replacements "favorite?$icon_favorite:" "$collection_name")
-# documents=$(lao ls -s "-title" -f "{id}\t{title}\0icon\x1f{favorite?$icon_favorite:}" "$collection_name")
+documents=$(lao ls \
+  -s "-title" \
+  -f "{id}\t{title} ({collection})\0icon\x1f{favorite}" \
+  --format-replacements "favorite?$icon_favorite:" \
+  "${collections[@]}")
 
-# optional flag for showing more info:  -mesg "<span color='#636e72'>4 Documents | 5 Collections</span>"
-doc=$(echo -en "$documents" | cut -d'	' -f2 | rofi -show-icons -dmenu -i -p "" -width 30% -theme-str "entry {placeholder: \"Search...\";}")
+doc=$(echo -en "$documents" | cut -d$'\t' -f2 | rofi \
+  -kb-custom-1 "Control+Alt+p" \
+  -show-icons \
+  -dmenu \
+  -i \
+  -p "" \
+  -width 30% \
+  -theme-str "entry {placeholder: \"Search...\";}")
+exit_code="$?"
 
-if [ -z "$doc" ];then exit; fi
+if [ -z "$doc" ]; then exit; fi
 
-doc_id=$(echo -en "$documents" | grep -wam1 "$doc" | cut -d'	' -f1)
-
+doc_id=$(echo -en "$documents" | grep -wam1 "$doc" | cut -d$'\t' -f1)
 if [ -z "$doc_id" ];then exit; fi
 
-doc_file=$(lao ls -f "{filepath}" -i "$doc_id")
-
-evince "$doc_file"
+case "$exit_code" in
+  "0")
+    doc_file=$(lao ls -f "{filepath}" -i "$doc_id")
+    evince "$doc_file"
+    ;;
+  "10")
+    lao ls --toggle-favorite -i "$doc_id" >/dev/null 2>&1
+    notify-send -i "favorite" "Toggle favorite for '$doc'"
+    ;;
+esac
--- scripts/.scripts/brightness.sh
+++ scripts/.scripts/brightness.sh
@@ -4,18 +4,13 @@
 # ./brightness up
 # ./brightness down
 
-ICON_DIR=$HOME/.config/i3/scripts/icons
-
-function get_current_brightness {
-	light -G
-}
-
 function send_notification {
-	level=`get_current_brightness`
-	#icon_name=$ICON_DIR/brightness-lcd.svg
-
-	notify-send -u low -i "gpm-brightness-lcd" "$level%" -t 700 -h string:x-canonical-private-synchronous:brightness_level
-	# notify-send -u low "$level%" -i "$icon_name" -t 700 -h string:x-canonical-private-synchronous:brightness_level
+	level=$(light -G)
+	level=$(printf "%.0f%%" $level)
+	notify-send -u low \
+		-i "gpm-brightness-lcd" "$level" \
+		-t 700 \
+		-h string:x-canonical-private-synchronous:brightness_level
 }
 
 case $1 in
